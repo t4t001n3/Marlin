ed11e74551c92c1ee87f7e3ff7d07459cfda8d22
t4t001n3 <t4t001n3@gmail.com>
2020-03-28 21:21:19 -0400

    Fix motherboard

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,7 +128,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define BOARD_MKS_BASE_14
+  #define MOTHERBOARD BOARD_MKS_GEN_13
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
