dd16046acda41b4b279cffd3a79438970e98f46e
t4t001n3 <t4t001n3@gmail.com>
2020-03-28 21:02:10 -0400

    Change MOTHERBOARD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -128,7 +128,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define BOARD_MKS_BASE_14
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
